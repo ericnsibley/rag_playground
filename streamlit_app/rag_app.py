f2b6a8b8b072b41654799c91dde48dda9770235c
ericnsibley <ericnsibley@gmail.com>
2024-08-03 15:20:58 -0500

    session state, durable db conn, CRUD calls to db, three query styles done and qa-ed

--- streamlit_app/rag_app.py
+++ streamlit_app/rag_app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import psycopg2
 import os 
 import util
 import logging
@@ -11,27 +10,29 @@ secret = os.getenv('OPENAI_API_KEY')
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+logger.info("Rerunning page...")
 
+util.get_db_connection()
 
-conn = util.get_db_connection()
-
-conversations = util.get_conversations(conn)
+conversations = util.get_conversations() 
 current_conversation = st.sidebar.selectbox("Select a conversation", conversations)
-logging.info(f"current_conversation: {current_conversation}")
 
 st.title("Conversation history testbench")
-conversation = st.text_input("Conversation", current_conversation if current_conversation else "")
 
-old_interactions = util.fetch_interactions(conn, conversation)
+st.text_input("Conversation", current_conversation if current_conversation else "", key='convo')
+
+if 'old_interactions' not in st.session_state:
+    st.session_state['old_interactions'] = util.fetch_interactions()
+
 messages_length = lambda i: len(i) if len(i) > 0 else 1
-num_messages = st.number_input("Number of messages in the conversation", min_value=1, max_value=20, value=messages_length(old_interactions), step=1)
+num_messages = st.number_input("Number of messages in the conversation", min_value=1, max_value=20, value=messages_length(st.session_state.old_interactions), step=1)
 interactions: list[util.Interaction] = []
 
 cols = st.columns(2)
 for i in range(num_messages):
-    if i < len(old_interactions):
-        human = old_interactions[i].human
-        bot = old_interactions[i].bot
+    if i < len(st.session_state.old_interactions):
+        human = st.session_state.old_interactions[i].human
+        bot = st.session_state.old_interactions[i].bot
     else:
         human = ""
         bot = ""
@@ -39,27 +40,29 @@ for i in range(num_messages):
         user_msg = st.text_area(f"Message {i+1}", human)
     with cols[1]:
         bot_msg = st.text_area(f"Response {i+1}", bot)
-    interactions.append(util.Interaction(human=user_msg, bot=bot_msg))
 
-interactions = util.memoize(old_interactions, interactions)
+    interaction = util.Interaction(human=user_msg, bot=bot_msg)
+    if interaction.human != "" and interaction.bot != "":
+        interactions.append( interaction )
 
-old_query = util.fetch_query(conn, conversation=conversation)
-if old_query:
-    q = old_query.human
-else:
+st.session_state['interactions'] = util.memoize(st.session_state['old_interactions'], interactions)
+st.write(len(st.session_state['interactions']))
+if 'old_query' not in st.session_state:
+    st.session_state['old_query'] = util.fetch_query()
+if st.session_state['old_query'] is None:
     q = ""
+else:
+    q = st.session_state['old_query'].human
+    
 query = st.text_area(f"Query", q)
-query_interaction = util.Interaction(human=query, bot="") # TODO
+st.session_state['query'] = util.Interaction(human=query, bot="") # TODO
 
-if st.button("Store Interactions"):
+
+if st.button("Store Interactions", on_click=util.store_and_search_interactions):
     if query.strip() == "":
-        st.warning("Query can't be empty!")
-    else:
-        util.wipe_table(conn)
-        util.store_interactions(conn=conn, interactions=interactions, conversation=conversation)
-        # search db 
-        util.store_query(conn=conn, query=query_interaction, conversation=conversation)
+        st.warning("Query can't be empty!") # This does nothing to a callback
+
 
 if st.button("Wipe Table"):
-    util.wipe_db(conn)
-    util.init_db(conn)
+    util.wipe_db()
+    util.init_db()
--- streamlit_app/util.py
+++ streamlit_app/util.py
@@ -6,36 +6,42 @@ import streamlit as st
 
 logger = logging.getLogger(__name__)
 
-@st.cache_resource
+# @st.cache_resource # Durable through faults is better than caching a connection
+# cache_resource on a pool is probably even better though
 def get_db_connection():
-    conn = psycopg2.connect(
-        host=os.getenv('PGVECTOR_HOST'),
-        port=os.getenv('PGVECTOR_PORT'),
-        database=os.getenv('PGVECTOR_DB'),
-        user=os.getenv('PGVECTOR_USER'),
-        password=os.getenv('PGVECTOR_PASSWORD')
-    )
-    init_db(conn)
-    return conn
+    if 'db_conn' not in st.session_state or st.session_state['db_conn'].closed:
+        logger.info("Getting new db connection.")
+        st.session_state['db_conn'] = psycopg2.connect(
+            host=os.getenv('PGVECTOR_HOST'),
+            port=os.getenv('PGVECTOR_PORT'),
+            database=os.getenv('PGVECTOR_DB'),
+            user=os.getenv('PGVECTOR_USER'),
+            password=os.getenv('PGVECTOR_PASSWORD')
+        )
+        init_db()
+    else: 
+        logger.info("Reusing db connection.")
 
 
-def init_db(conn, create_file: str = "create_table.sql"):
+def init_db(create_file: str = "create_table.sql"):
     logger.info("Initializing db")
     with open(create_file, "r") as infile: 
         sql = infile.read()
-    with conn.cursor() as cur:
+    with st.session_state['db_conn'].cursor() as cur:
         cur.execute(sql)
-        conn.commit()
+        st.session_state['db_conn'].commit()
 
 
-def wipe_db(conn):
-    with conn.cursor() as cur:
+def wipe_db():
+    logger.info(f"Wiping db.")
+    with st.session_state['db_conn'].cursor() as cur:
         cur.execute("DROP TABLE Conversations;")
-        conn.commit()
+        st.session_state['db_conn'].commit()
 
 
-def get_conversations(conn) -> list[str]:
-    with conn.cursor() as cur:
+def get_conversations() -> list[str]:
+    logger.info(f"Getting distinct conversations")
+    with st.session_state['db_conn'].cursor() as cur:
         cur.execute("SELECT DISTINCT conversation FROM Conversations;")
         rows = cur.fetchall()
     return [ row[0] for row in rows ]
@@ -59,7 +65,7 @@ Bot: {self.bot}"""
     
     def get_tokens(self) -> int:
         if self.tokens:
-            return
+            return self.tokens
         encoding = tiktoken.get_encoding('cl100k_base')
         tokens = len(encoding.encode(str(self)))
         self.tokens = tokens 
@@ -67,100 +73,98 @@ Bot: {self.bot}"""
     
     def get_embedding(self) -> list[float]: # TODO: skipped for now for cost
         if self.embedding:
-            return
+            return self.embedding
         embedding = [ i/10 for i in range(1536) ]
         self.embedding = embedding 
         return embedding 
 
 
-def wipe_table(conn):
-    with conn.cursor() as cur:
+def wipe_table():
+    logging.info("Wiping table.")
+    with st.session_state['db_conn'].cursor() as cur:
         cur.execute("DELETE FROM Conversations;")
-        conn.commit()
+        st.session_state['db_conn'].commit()
 
 
-def store_interactions(conn, interactions: list[Interaction], conversation: str):
-    with conn.cursor() as cur:
-        for i in interactions:
+def store_interactions():
+    with st.session_state['db_conn'].cursor() as cur:
+        for i in st.session_state['interactions']:
             sql = """
 INSERT INTO Conversations ("content", content_vector, token_count, conversation, result)
 VALUES (%s, %s, %s, %s, %s)"""
-            cur.execute(sql, (str(i), i.get_embedding(), i.get_tokens(), conversation, False))
-            conn.commit()
-    logging.info(f"{len(interactions)} stored in db.")
+            cur.execute(sql, (str(i), i.get_embedding(), i.get_tokens(), st.session_state.convo, False))
+            st.session_state['db_conn'].commit()
+    logger.info(f"{len(st.session_state['interactions'])} stored in db.")
 
 
-def store_query(conn, query: Interaction, conversation: str): 
-    with conn.cursor() as cur:
+def store_query(): 
+    with st.session_state['db_conn'].cursor() as cur:
         sql = """
 INSERT INTO Conversations (content, content_vector, token_count, conversation, result)
 VALUES (%s, %s, %s, %s, %s)"""
-        cur.execute(sql, (str(query), query.get_embedding(), query.get_tokens(), conversation, True))
-        conn.commit()
-    logging.info(f"Query stored in db.")
+        cur.execute(sql, (str(st.session_state['query']), st.session_state['query'].get_embedding(), st.session_state['query'].get_tokens(), st.session_state.convo, True))
+        st.session_state['db_conn'].commit()
+    logger.info(f"Query stored in db.")
 
 
-def fetch_interactions(conn, conversation: str) -> list[Interaction]:
-    with conn.cursor() as cur:
+def fetch_interactions() -> list[Interaction]:
+    with st.session_state['db_conn'].cursor() as cur:
         cur.execute("""SELECT 
     content, 
-    content_vector,
     token_count,
+    content_vector,
     result
 FROM Conversations
 WHERE conversation=%s
-    AND result=False;""", (conversation,))
+    AND result=False
+ORDER BY timestamp;""", (st.session_state.convo,))
         rows = cur.fetchall()
-    interactions = []
-    for row in rows: 
-        print(f"row: {row}")
-        data = {
-            "human": row[0].split('\nBot: ')[0].replace('Human: ', ''),
-            "bot": row[0].split('\nBot: ')[1],
-            "embedding": [ float(i) for i in row[1].replace('[', '').replace(']', '').split(',') ],
-            "tokens": row[2],
-            "result": row[3]
-        }
-        interactions.append( Interaction.from_dict(data) )
+    interactions = db_rows_to_interactions(rows)
     if interactions:
-        logging.info(f"Fetched {len(interactions)} interactions in conversation {conversation}.")
+        logger.info(f"Fetched {len(interactions)} interactions in conversation {st.session_state.convo}.")
     else:
-        logging.info(f"New conversation.")
+        logger.info(f"No interactions to fetch, new conversation.")
     return interactions 
 
 
-def fetch_query(conn, conversation: str) -> Interaction | None:
-    with conn.cursor() as cur:
+def fetch_query() -> Interaction | None:
+    with st.session_state['db_conn'].cursor() as cur:
         cur.execute("""SELECT 
     content, 
-    content_vector,
     token_count,
+    content_vector,
     result
 FROM Conversations
 WHERE conversation=%s
-    AND result=True;""", (conversation,))
+    AND result=True;""", (st.session_state.convo,))
         rows = cur.fetchall()
+    interactions = db_rows_to_interactions(rows)
+    ret = None
+    if interactions:
+        logger.info(f"Fetched {len(interactions)} query in conversation {st.session_state.convo}.")
+        assert len(interactions) == 1
+        ret = interactions[0]
+    else:
+        logger.info(f"No query, new conversation.")
+    return ret 
+
+
+row_get = lambda row, index, default=None: row[index] if index < len(row) else default
+def db_rows_to_interactions(rows) -> list[Interaction]:
     interactions = []
     for row in rows: 
         data = {
             "human": row[0].split('\nBot: ')[0].replace('Human: ', ''),
             "bot": row[0].split('\nBot: ')[1],
-            "embedding": [ float(i) for i in row[1].replace('[', '').replace(']', '').split(',') ],
-            "tokens": row[2],
-            "result": row[3]
+            "tokens": row[1],
+            "embedding": [ float(i) for i in row[2].replace('[', '').replace(']', '').split(',') ] if row_get(row, 2) else None,
+            "result": row_get(row, 3)
         }
         interactions.append( Interaction.from_dict(data) )
-    ret = None
-    if interactions:
-        logging.info(f"Fetched {len(interactions)} interactions in conversation {conversation}.")
-        assert len(interactions) == 1
-        ret = interactions[0]
-    else:
-        logging.info(f"New conversation.")
-    return ret 
+    return interactions
 
 
-def memoize(old: list[Interaction], new: list[Interaction]) -> list[Interaction]:
+def memoize(old: list[Interaction], new: list[Interaction]) -> list[Interaction]: # optimize the common case to reduce costs
     ret = []
     o_len = len(old)
     n_len = len(new)
@@ -171,3 +175,142 @@ def memoize(old: list[Interaction], new: list[Interaction]) -> list[Interaction]
         else:
             ret.append( new[i] )
     return ret 
+
+
+MAX_TOKENS = 500
+def search_by_similarity(embedding: list[float], max_tokens: int = MAX_TOKENS) -> list[Interaction]:
+    with st.session_state['db_conn'].cursor() as cur:
+        cur.execute("""
+WITH Similar_messages AS (
+    SELECT 
+        content,
+        token_count,
+        SUM(token_count) OVER (ORDER BY content_vector <=> %s::vector ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS cumulative_tokens 
+    FROM Conversations
+    WHERE conversation = %s
+        AND result = False
+)
+SELECT
+    content, 
+    token_count
+FROM Similar_messages
+WHERE cumulative_tokens <= %s;""", (embedding, st.session_state.convo, max_tokens)) 
+        rows = cur.fetchall()
+    interactions = db_rows_to_interactions(rows)
+    if interactions:
+        logger.info(f"Similarity search fetched {len(interactions)} interactions in conversation {st.session_state.convo}.")
+    else:
+        logger.info(f"No conversation to search.")
+    return interactions 
+
+
+def search_by_recent(max_tokens: int = MAX_TOKENS) -> list[Interaction]:
+    with st.session_state['db_conn'].cursor() as cur:
+        cur.execute("""
+WITH Ranked_conversations AS (
+    SELECT 
+        content,
+        token_count,
+        SUM(token_count) OVER (ORDER BY timestamp DESC ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS cumulative_tokens 
+    FROM Conversations
+    WHERE conversation = %s
+        AND result = False
+)
+SELECT 
+    content, 
+    token_count
+FROM Ranked_conversations
+WHERE cumulative_tokens <= %s;""", (st.session_state.convo, max_tokens)) 
+        rows = cur.fetchall()
+    interactions = db_rows_to_interactions(rows)
+    if interactions:
+        logger.info(f"Recency search fetched {len(interactions)} interactions in conversation {st.session_state.convo}.")
+    else:
+        logger.info(f"No conversation to search.")
+    return interactions 
+
+
+def search_by_recent_and_similarity(embedding: list[float], recent_message_limit: int = 1, max_tokens: int = MAX_TOKENS) -> list[Interaction]:
+    with st.session_state['db_conn'].cursor() as cur:
+        cur.execute("""
+WITH Recent_messages AS (
+    SELECT 
+        content,
+        token_count,
+        timestamp,
+        SUM(token_count) OVER (ORDER BY timestamp DESC ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS cumulative_tokens
+    FROM Conversations
+    WHERE conversation=%s
+        AND result=False
+    LIMIT %s
+),
+Filtered_recent_messages AS (
+    SELECT 
+        content,
+        token_count,
+        timestamp,
+        cumulative_tokens
+    FROM Recent_messages
+    WHERE cumulative_tokens <= %s
+),
+Recent_token_sum AS (
+    SELECT COALESCE(SUM(token_count), 0) AS tokens FROM Filtered_recent_messages
+),
+Similar_messages AS (
+    SELECT 
+        content,
+        token_count,
+        SUM(token_count) OVER (ORDER BY content_vector <=> %s::vector ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS cumulative_tokens
+    FROM Conversations
+    WHERE conversation = %s
+        AND result = False
+        AND timestamp NOT IN (SELECT timestamp FROM Filtered_recent_messages)
+),
+Filtered_similar_messages AS (
+	SELECT
+		content, 
+		token_count,
+		cumulative_tokens
+	FROM Similar_messages, Recent_token_sum
+	where cumulative_tokens + Recent_token_sum.tokens <= %s
+)
+SELECT 
+    content,
+    token_count
+FROM Filtered_recent_messages
+UNION ALL
+SELECT 
+    content,
+    token_count
+FROM Filtered_similar_messages;""", (st.session_state.convo, recent_message_limit, max_tokens, embedding, st.session_state.convo, max_tokens)) 
+        rows = cur.fetchall()
+    interactions = db_rows_to_interactions(rows)
+    if interactions:
+        logger.info(f"Blended search fetched {len(interactions)} interactions in conversation {st.session_state.convo}.")
+    else:
+        logger.info(f"No conversation to search.")
+    return interactions 
+
+
+def log(interactions: list[Interaction]):
+    for i, j in enumerate(interactions):
+        logger.info(f"Interaction: {i} content length: {len(str(j))}")
+        logger.info(f"Tokens: {j.get_tokens()}")
+    print()
+
+
+def store_and_search_interactions():
+    wipe_table()
+    store_interactions()
+
+    if st.session_state['query'].human.strip() != "":
+        interactions = search_by_recent()
+        log(interactions)
+        logger.info(f"recent")
+        interactions = search_by_similarity(st.session_state['query'].get_embedding())
+        logger.info(f"similarity")
+        log(interactions)
+        interactions = search_by_recent_and_similarity(st.session_state['query'].get_embedding())
+        logger.info(f"recent_and_similarity")
+        log(interactions)
+        store_query()
